4bbc8999cc4971d1186143c9366cd11531a90e2d
Rahul-0410 <rgs786999@gmail.com>
2024-10-24 18:15:44 +0530

    tenth

---
+++ src/components/slider/Slider.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './slider.scss';
+
+function Slider() {
+  return (
+    <div>Slider</div>
+  )
+}
+
+export default Slider
--- src/routes/singlePage/SinglePage.jsx
+++ src/routes/singlePage/SinglePage.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './singlePage.scss';
+import Slider from '../../components/slider/Slider';
 
 function SinglePage() {
   return (
-    <div>SinglePage</div>
+    <div className='singlePage'>
+        <div className="details">
+          <div className="wrapper">
+            <Slider/>
+            <div className="info">
+              
+            </div>
+          </div>
+        </div>
+        <div className="features">
+          <div className="wrapper"></div>
+        </div>
+    </div>
   )
 }
 
