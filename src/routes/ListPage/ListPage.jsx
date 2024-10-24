073db6868928cb3f24c4236d9d44e0c047360325
Rahul-0410 <rgs786999@gmail.com>
2024-10-24 17:15:02 +0530

    tenth

--- src/routes/ListPage/ListPage.jsx
+++ src/routes/ListPage/ListPage.jsx
@@ -3,6 +3,7 @@ import './listpage.scss';
 import { listData } from '../../lib/dummyData';
 import Filter from '../../components/filter/Filter';
 import Card from '../../components/card/Card';
+import Map from '../../components/map/Map';
 
 function ListPage() {
 
@@ -20,7 +21,9 @@ function ListPage() {
            }
         </div>
       </div>
-      <div className="mapContainer">Map</div>
+      <div className="mapContainer">
+        <Map items={data}/>
+      </div>
     </div>
   )
 }
