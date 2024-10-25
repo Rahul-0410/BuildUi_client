73972e5560ad0c02f53e0fb09841625f3f873e97
Rahul-0410 <rgs786999@gmail.com>
2024-10-25 22:36:59 +0530

    eleventh

--- src/routes/singlePage/SinglePage.jsx
+++ src/routes/singlePage/SinglePage.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import './singlePage.scss';
 import Slider from '../../components/slider/Slider';
+import { singlePostData, userData } from '../../lib/dummyData';
 
 function SinglePage() {
   return (
     <div className='singlePage'>
         <div className="details">
           <div className="wrapper">
-            <Slider/>
+            <Slider images={singlePostData.images}/>
             <div className="info">
-              
+              <div className="top">
+                <div className="post">
+                  <h1>{singlePostData.title}</h1>
+                  <div className="address">
+                    <img src="/pin.png" alt="pin" />
+                    <span>{singlePostData.address}</span>
+                  </div>
+                  <div className="price">
+                    $ {singlePostData.price}
+                  </div>
+                </div>
+                  <div className="user">
+                    <img src={userData.img} alt="" />
+                    <span>{userData.name}</span>
+                  </div>
+              </div>
+              <div className="bottom"></div>
             </div>
           </div>
         </div>
