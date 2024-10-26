6ce482e3ceeaa731713a5a823544b39bfd58bfda
Rahul-0410 <rgs786999@gmail.com>
2024-10-26 17:02:57 +0530

    eleventh

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -3,7 +3,30 @@ import './slider.scss';
 
 function Slider({images}) {
   return (
-    <div>Slider</div>
+    <div className='slider'>
+      <div className="fullSlider">
+        <div className="arrow">
+          <img src="/arrow.png" alt="arrow" />
+        </div>
+        <div className="imageContainer">
+          <img src={images[0]} alt="" />
+        </div>
+        <div className="arrow">
+          <img src="./arrow.png" className='right' alt="arrow" />
+        </div>
+        <div className="close">
+          X
+        </div>
+      </div>
+      <div className="bigImage">
+        <img src={images[0]} alt="" />
+      </div>
+      <div className="smallImage">
+        {images.slice(1).map((image,index)=>(
+          <img src={image} key={index}/>
+        ))}
+      </div>
+    </div>
   )
 }
 
