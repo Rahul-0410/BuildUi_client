86128722a6432ff0c80979b80f676a3f79fad54a
Rahul-0410 <rgs786999@gmail.com>
2024-11-03 13:31:12 +0530

    14

--- src/routes/register/Register.jsx
+++ src/routes/register/Register.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './register.scss';
 import { Link } from 'react-router-dom';
+import axios from "axios"
 
 function RegisterPage() {
+  const [err,setError]= useState("");
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    const formData= new FormData(e.target);
+
+    const username= formData.get("username");
+    const email= formData.get("email");
+    const password= formData.get("password");
+
+    try{
+      const res= await axios.post("http://localhost:3001/api/auth/register",{
+        username,email,password
+      })
+      console.log(res.data);
+      
+    }catch(err){
+      console.log(err);
+      
+    }
+    
+  }
   return (
     <div className="register">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Create an Account</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
