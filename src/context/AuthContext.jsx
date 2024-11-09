8713d913857d8149e8e8e3d220b94dc6887317d3
Rahul-0410 <rgs786999@gmail.com>
2024-11-09 23:48:23 +0530

    go

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,7 +3,9 @@ import { createContext, useState } from "react";
 export const AuthContext = createContext();
 
 export const AuthContextProvider= ({children})=>{
-    const [currentUser,setCurrentUser]= useState(;
+    const [currentUser,setCurrentUser]= useState(
+        JSON.parse(localStorage.getItem("user")) || null
+    );
 
     return(
         <AuthContext.Provider >{children}</AuthContext.Provider>
